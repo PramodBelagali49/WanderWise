9c2f4d3622495fe401b88d08c0194a7e3f65ea29
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-03-13 20:21:16 +0530

    defined model for db

---
+++ models/listing.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema= new Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type:String,
+    },
+    price:{
+        type:Number,
+    },
+    image: {
+        filename: { type: String, required: true },
+        url: { 
+            type: String, 
+            default: "https://media.istockphoto.com/id/2157387146/photo/contemporary-charm-exploring-the-exterior-of-the-modern-cottage-villa.webp?a=1&b=1&s=612x612&w=0&k=20&c=_ECmcv8NS-dwpDQo_QBH_4aFPgOScm_oaGvdl1ytmCk=",
+            set: (v) => v === "" ? "https://media.istockphoto.com/id/2157387146/photo/contemporary-charm-exploring-the-exterior-of-the-modern-cottage-villa.webp?a=1&b=1&s=612x612&w=0&k=20&c=_ECmcv8NS-dwpDQo_QBH_4aFPgOScm_oaGvdl1ytmCk=" : v  
+        }
+    },
+    location:{
+        type:String
+    },
+    country:{
+        type:String
+    }
+})
+
+const Listing=mongoose.model("Listing",listingSchema);
+
+module.exports=Listing;
