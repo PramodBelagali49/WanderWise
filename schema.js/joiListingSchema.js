edc06ce1f886c3ebe3dc472fe8d91c6721b02397
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-03-26 06:15:47 +0530

    added server side form validation using Joi npm package

---
+++ schema.js/joiListingSchema.js
@@ -0,0 +1,19 @@
+const Joi=require("joi");
+
+const listingSchema=Joi.object({
+    title: Joi.string().required(),
+
+    description : Joi.string()
+                    .required()
+                    .max(40),
+
+    price: Joi.number().min(0).required(),
+
+    location:Joi.string().required(),
+
+    country: Joi.string().required(),
+
+    image: Joi.string().allow("",null)
+})
+
+module.exports=listingSchema;
