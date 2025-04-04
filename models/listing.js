c162ecd89a0c48c61fe62c3bd9cbf5e631af2202
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-04-04 07:28:51 +0530

    established 1-to-n relation b/w listing & review models

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { type } = require("../schema.js/joiListingSchema");
+const { ref } = require("joi");
 const Schema = mongoose.Schema;
 
 const listingSchema= new Schema({
@@ -27,7 +29,13 @@ const listingSchema= new Schema({
     },
     country:{
         type:String
-    }
+    },
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review"
+        }
+    ]
 })
 
 const Listing=mongoose.model("Listing",listingSchema);
