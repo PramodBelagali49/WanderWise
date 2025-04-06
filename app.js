b7cecd0e72d4ba732a4bfd3cebf310ee36638737
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-04-06 14:15:33 +0530

    experimented with cookies

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
-const Listing=require("./models/listing.js");
-
-const wrapAsync=require("./utils/wrapAsync.js")
 const ExpressError=require("./utils/ExpressError.js")
 
 const methodOverride=require("method-override");
@@ -13,16 +10,10 @@ app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
-
 app.use(express.static(path.join(__dirname,"/public"))); 
 
 const ejsMate=require("ejs-mate");
 app.engine("ejs",ejsMate);
-
-// const listingSchema=require("./schema.js/joiListingSchema.js");
-const {listingSchema,reviewSchema}=require("./Joi_Schema.js")
-
-const Review = require("./models/review.js");
 
 async function  main(){
     try{
@@ -34,11 +25,9 @@ async function  main(){
 }
 main();  // Calling main() after defining it is better than calling it before , although it works fine
 
-
 // Listings related all routes imported from listingsRoutes.js file
 const listingroutes=require("./routes/listingsRoutes.js")
 app.use("/listings",listingroutes);
-
 
 // Reviews related all routes imported from reviewsRoutes.js file
 const revroutes=require("./routes/reviewsRoutes.js")
@@ -47,6 +36,35 @@ app.use("/listings/:id/reviews",revroutes);
 app.get("/",(req,resp)=>{
     resp.send("Welcome to home page bhai");
 })
+
+
+// COOKIES
+// requiring cookie-parser npm package
+const cookieParser=require("cookie-parser");
+app.use(cookieParser("**##secretkey##**"));
+
+app.get("/setCookies",(req,resp)=>{
+    resp.cookie("name","Pramod")
+    resp.send("Cookie sent successfully!!");
+})
+
+app.get("/getSignedCookies",(req,resp)=>{
+    resp.cookie("country","Bharat",{signed:true});
+    resp.send("Signed cookie sent successfully!!");
+})
+
+// verify signed cookies
+app.get("/verify",(req,resp)=>{
+    resp.send(req.signedCookies);  
+    // if value is modified then false is sent as repsonse else the original values is sent in the response
+    // if value is completely erased (made empty) then {} is returned
+})
+
+app.get("/getCookies",(req,resp)=>{
+    let {name}=req.cookies;
+    resp.send(`<h1>Hello ${name} !!</h1`);
+})
+
 
 app.all("*",(req,resp,next)=>{
     next(new ExpressError(404,"PaGe NoT FoUnD"));
