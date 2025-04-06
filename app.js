e9a00b90e28848c988eb46491ee72e3038154ebd
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-04-06 13:10:29 +0530

    separated listings and review routes from app.js to individual files

--- app.js
+++ app.js
@@ -35,125 +35,14 @@ async function  main(){
 main();  // Calling main() after defining it is better than calling it before , although it works fine
 
 
+// Listings related all routes imported from listingsRoutes.js file
+const listingroutes=require("./routes/listingsRoutes.js")
+app.use("/listings",listingroutes);
 
-// SERVER SIDE VALIDATION FOR LISTING
-const validateListing=(req,resp,next)=>{
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
 
-// SERVER SIDE VALIDATION FOR REVIEW
-const validateReview=(req,res,next)=>{
-    let {error} = reviewSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errMsg);
-    }else{
-        next();
-    }
-}
-
-// DELETE ROUTE     (btn in showListing.ejs) 
-app.delete("/listings/:id",wrapAsync(async(req,resp)=>{
-    let {id}=req.params;
-    const deleted=await Listing.findByIdAndDelete(id,{new:true});
-    // console.log("DELETED LISTING: ",deleted);
-    resp.redirect("/listings");
-}))
-
-// UPDATE ROUTE 
-app.patch("/listings/:id",wrapAsync(async(req,resp)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"Invalid data for listing");
-    }
-    let {id}=req.params;
-    console.log("id received from edit page: ",id);
-    console.log("Request body: ",req.body);
-    let updatedListing={...req.body.listing};
-    const updated= await Listing.findByIdAndUpdate(id,updatedListing,{new:true});
-    console.log("UPDATED DATA: ",updated);
-    resp.redirect(`/listings/${id}`)
-}))
-
-// EDIT ROUTE     (btn in showListing.ejs)
-app.get("/listings/:id/edit",wrapAsync(async(req,resp)=>{
-    let {id}=req.params;
-    let listingData=await Listing.findById(id);
-    console.log("EDIT LISTING DATA: ",listingData);
-    resp.render("./listings/editListing.ejs",({listingData:listingData}));
-}))
-
-// CREATE NEW LISTING ROUTE 
-app.post("/listings", validateListing , wrapAsync(async (req,resp,next)=>{   // validateListing as middleware for server side validation
-    // console.log(req.body);
-
-    let result=listingSchema.validate(req.body);                   // SERVER SIDE VALIDATION USING JOI PACKAGE
-    // console.log(result.error)
-    if(result.error){
-        throw new ExpressError(400,result.error); 
-    }
-
-    let listing=req.body;
-    const newListing=new Listing(listing);
-    const addedListing = await newListing.save();
-    // console.log("New listing added: ",addedListing);
-    resp.redirect("/listings");
-}))
-
-// Display form for new listing input
-app.get("/listings/new",(req,resp)=>{
-    resp.render("./listings/newListing.ejs");
-})
-
-// SHOW ROUTE 
-app.get("/listings/:id",wrapAsync(async(req,resp)=>{
-    let {id}=req.params;
-    let listingData=await Listing.findById(id).populate("reviews");  // populate to get details of the reviews
-    console.log("SHOW LISTING DATA: ",listingData);
-    resp.render("./listings/showListing.ejs",{listingData:listingData});
-}))
-
-// INDEX ROUTE
-app.get("/listings", wrapAsync(async (req,resp)=>{
-    let allListings=await Listing.find({});
-    // console.log(allListings);
-    resp.render("./listings/index.ejs",{allListings:allListings});
-}))
-
-// REVIEW POST ROUTE
-app.post("/listings/:id/reviews", validateReview , wrapAsync(async(req,resp)=>{    // vallidateReview as middleware for server side validation
-    let {id}=req.params;
-            // OR 
-    // let id=req.params.id;
-
-    // console.log("review id: ",id);
-    let listing=await Listing.findById(id);
-    // console.log("listing data from review ",listing);
-    // console.log("review req body ",req.body);
-    let newReview=new Review(req.body.review);
-    listing.reviews.push(newReview);
-
-    let res=await newReview.save();
-    console.log("New review: ",res);
-    await listing.save();
-    resp.redirect(`/listings/${id}`);
-}))
-
-// REVIEW DELETE ROUTE
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,resp)=>{
-    let {id,reviewId}=req.params;
-
-    let res1=await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});  // pull operator to delete a review from reviews array
-    let res2=await Review.findByIdAndDelete(reviewId);
-
-    console.log("Review deleted: ",res1,res2);
-    resp.redirect(`/listings/${id}`);
-}))
+// Reviews related all routes imported from reviewsRoutes.js file
+const revroutes=require("./routes/reviewsRoutes.js")
+app.use("/listings/:id/reviews",revroutes);
 
 app.get("/",(req,resp)=>{
     resp.send("Welcome to home page bhai");
