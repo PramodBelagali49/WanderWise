c4e5d9ad0ea1cb45f427c08cc0eafafc9247639f
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-04-10 16:30:54 +0530

    added middlewares to check if the user is logged in & if he is the owner

---
+++ middlewares.js
@@ -0,0 +1,30 @@
+const Listing = require("./models/listing");
+
+module.exports.isLoggedIn=(req,resp,next)=>{
+    // console.log("req.user(in middleware.js): " , req.user);
+    // console.log(req);
+    if(!req.isAuthenticated()){
+        req.session.redirectUrl=req.originalUrl;
+        req.flash("error","You must be logged in");
+        return resp.redirect("/login");
+    }
+   // console.log("req.user(in middleware.js) after login: ",req.user);
+    next();
+};
+
+module.exports.saveRedirectUrl=(req,resp,next)=>{
+    if(req.session.redirectUrl){
+        resp.locals.redirectUrl=req.session.redirectUrl;
+    }
+    next();
+}
+
+module.exports.isOwner = async(req,resp,next)=>{
+    let {id}=req.params;
+    let listing=await Listing.findById(id);
+    if(!listing.owner._id.equals(resp.locals.currUser._id)){
+        req.flash("error","You don't have permission to modify/delete the Listing!!");
+        return resp.redirect(`/listings/${id}`);      // return is IMPORTANT here
+    }
+    next();
+}
