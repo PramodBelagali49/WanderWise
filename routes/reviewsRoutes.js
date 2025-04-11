caae9297ad2b5893c60f5ca93cf5e24ab9d37d43
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-04-11 10:18:17 +0530

    added author attribute to review schema

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync");
 const Review = require("../models/review");
 const Listing = require("../models/listing");
 const {reviewSchema}=require("../Joi_Schema.js");
+const { isLoggedIn, isAuthor } = require("../middlewares.js");
 
 // SERVER SIDE VALIDATION FOR REVIEW
 const validateReview=(req,res,next)=>{
@@ -17,7 +18,7 @@ const validateReview=(req,res,next)=>{
 }
 
 // REVIEWS POST ROUTE
-router.post("/", validateReview , wrapAsync(async(req,resp)=>{    // vallidateReview as middleware for server side validation
+router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,resp)=>{    // vallidateReview as middleware for server side validation
     let {id}=req.params;    // here the id from req.params is being fetched from parent route for reviews in app.js ( bcz of {mergeParams:true} )
             // OR 
     // let id=req.params.id;
@@ -27,6 +28,8 @@ router.post("/", validateReview , wrapAsync(async(req,resp)=>{    // vallidateRe
     console.log("listing data from review ",listing);
     console.log("review req body ",req.body);
     let newReview=new Review(req.body.review);
+    newReview.author=req.user._id;
+    console.log("newReview: ",newReview);
     listing.reviews.push(newReview);
 
     let res=await newReview.save();
@@ -38,7 +41,7 @@ router.post("/", validateReview , wrapAsync(async(req,resp)=>{    // vallidateRe
 }))
 
 // REVIEWS DELETE ROUTE
-router.delete("/:reviewId",wrapAsync(async(req,resp)=>{
+router.delete("/:reviewId",isLoggedIn,isAuthor,wrapAsync(async(req,resp)=>{
     let {id,reviewId}=req.params;
 
     let res1=await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});  // pull operator to delete a review from reviews array
