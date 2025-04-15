ce0c0a976d4f8ec99e2f562b307032527f112a7c
PramodBelagali49 <cse.pramod.b2@gmail.com>
2025-04-15 22:05:22 +0530

    directly converted location to coordinates using geocoding

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -1,25 +1,45 @@
 mapboxgl.accessToken = mapToken;
+const geocodingClient = mapboxSdk({ accessToken: mapToken }).geocoding;
+
+// for map-box
+// import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
+// const mapToken=process.env.MAP_BOX_TOKEN ;
+// const geocodingClient = mbxGeocoding({ accessToken: mapToken});
 
 // console.log("coordinates: ",coordinates)
 // let lng=coordinates[0];
 // let lat=coordinates[1];
 // let listingData=JSON.parse(listingData);   // listingData inside parenthesis is from showListing.ejs
 
-const map = new mapboxgl.Map({
-    container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: listingData.geometry.coordinates,     // JSON.parse converts the coordinates string to array or object
-    zoom: 5
-});
 
-// Create a default Marker and add it to the map.
-const marker = new mapboxgl.Marker({color:"red"})
-    .setLngLat(listingData.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setHTML(
-            `<h6>${listingData.location},${listingData.country}</h6> <p>Exact location is provided after booking</p>`
+async function geocode(){
+    let response=await geocodingClient.forwardGeocode({
+        query: listingData.location ,
+        limit: 1
+    })
+    .send();
+    let coordinates=response.body.features[0].geometry.coordinates;
+    console.log("coordinates: ",coordinates);
+
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: coordinates,     // JSON.parse converts the coordinates string to array or object
+        zoom: 6
+    });
+    
+    // Create a default Marker and add it to the map.
+    const marker = new mapboxgl.Marker({color:"red"})
+        .setLngLat(coordinates)
+        .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML(
+                `<h6>${listingData.location} , ${listingData.country}</h6> <p>Exact location is provided after booking</p>`
+            )
         )
-    )
-    .addTo(map)
+        .addTo(map)
+}
+geocode();
+
+
